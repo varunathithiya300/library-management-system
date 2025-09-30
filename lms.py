fcc150185a9191da11295cb21ed2a2906bc80bba
Varun Shenbagaraj <shenbagarajvarun@gmail.com>
2025-09-30 23:16:22 +0530

    sync up

--- lms.py
+++ lms.py
@@ -18,22 +18,32 @@ class member:
         if not member_db.get(self.firstname):
             member_db[self.firstname] = {'firstname':self.firstname, 'lastname':self.lastname, 'dob':self.dob, 'email':self.email}
 
-    def donate_book(self):
-        if not books_db.get(self.title): # figure out a way to add a nested struct containing the book information to books_db
-            books_db[self.title] = {'author':self.author, 'isbn':self.isbn, 'is_available':'y'}
+    def donate_book(self, title, author, isbn):
+        if not books_db.get(title): # figure out a way to add a nested struct containing the book information to books_db
+            books_db[title] = {'author':author, 'isbn':isbn, 'is_available':'y'}
         else:
             pass
 
     def borrow_book(self, title, author, isbn):
+        """ 
+        whenever a members borrows a book, the is_available flag must be changed to 'N'
+        We should also maintain a mapping of which book is with which member
+        """
         pass
 
 
 m1 = member('Varun', 'Athithiya', '02041994', 'shenbagarajvarun@gmail.com')
-# m1.donate_book('abc', 'varun', '123456789')
-# m1.donate_book('Fluent Python', 'Luciano Ramalho', '978-1492056355')
-# m1.donate_book('Data Mining', 'Jiawei Han and Micheline Kamber', '978-8131205358')
+m1.register_member()
+
 m2 = member('Kirtana', 'Shenbagaraj', '31122001', 'kirtanasraj@gmail.com')
 m2.register_member()
-m1.register_member()
-# print(books_db)
-print(member_db.items())
+
+m1.donate_book('abc', 'varun', '123456789')
+m1.donate_book('Fluent Python', 'Luciano Ramalho', '978-1492056355')
+m1.donate_book('Data Mining', 'Jiawei Han and Micheline Kamber', '978-8131205358')
+
+for k,v  in books_db.items():
+    print(k, v)
+
+for k,v  in member_db.items():
+    print(k, v)
