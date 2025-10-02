9515afb94f066aef84067f2d22f844ea85fbbab2
Varun Shenbagaraj <shenbagarajvarun@gmail.com>
2025-10-02 14:54:56 +0530

    added book donation date

--- lms.py
+++ lms.py
@@ -2,18 +2,22 @@ from datetime import datetime, timedelta
 from dataclasses import dataclass
 import time
 
+book_donation_date = datetime.now().strftime("%Y-%m-%d")
 member_db = {}
 books_db = {}
 
 
 class book:
-    def __init__(self, title: str, author: str, isbn: str):
-        self.isbn = isbn
+    def __init__(self, title: str, author: str, isbn: str, donated_date: datetime.date) -> object:
+        """ 
+        The library can have up to 5 copies of the same book to cater to any demand. (not implemented yet)  
+        """
         self.title = title
         self.author = author
-        self.donated_date = datetime.now().strftime("%Y-%m-$d")
+        self.isbn = isbn
+        self.donated_date = donated_date
 
-class member:
+class member():
     def __init__(self, firstname: str, lastname: str, dob: str, email: str):
         self.firstname = firstname
         self.lastname = lastname
@@ -26,11 +30,11 @@ class member:
         if not member_db.get(self.firstname):
             member_db[self.firstname] = {'firstname':self.firstname, 'lastname':self.lastname, 'dob':self.dob, 'email':self.email, 'doj': self.doj, 'dor': self.dor}
 
-    def donate_book(self, title, author, isbn):
+    def donate_book(self, title, author, isbn, donated_date):
         if books_db.get(title): # figure out a way to add a nested struct containing the book information to books_db
-            pass
+            print(f'{title} already available. Please donate a different book. Thank you. Keep contributing :)')
         else:
-            books_db[title] = {'author':author, 'isbn':isbn, 'is_available':'y'}
+            books_db[title] = {'author':author, 'isbn':isbn, 'is_available':'y', 'donated_date': donated_date}
 
     def borrow_book(self, book_title):
         """ 
@@ -63,13 +67,22 @@ m1.register_member()
 m2 = member('Kirtana', 'Shenbagaraj', '31122001', 'kirtanasraj@gmail.com')
 m2.register_member()
 
-m1.donate_book('abc', 'varun', '123456789')
-m1.donate_book('Fluent Python', 'Luciano Ramalho', '978-1492056355')
-m1.donate_book('Data Mining', 'Jiawei Han and Micheline Kamber', '978-8131205358')
+m1.donate_book('Designing Data-Intensive Applications', 'Martin Kleppmann', '978-9352135240', book_donation_date)
+m1.donate_book('Fluent Python', 'Luciano Ramalho', '978-1492056355', book_donation_date)
+m1.donate_book('Data Mining', 'Jiawei Han and Micheline Kamber', '978-8131205358', book_donation_date)
+m2.donate_book('Database Internals: A Deep Dive Into How Distributed Data Systems Work', 'Alex Petrov', '978-9352139071', book_donation_date )
 
-m1.borrow_book('Fluent Python')
-m2.borrow_book('Fluent Python')
-m2.borrow_book('Data Mining')
-m1.borrow_book('Data Mining')
-print(books_db)
+# m1.borrow_book('Fluent Python')
+# m2.borrow_book('Fluent Python')
+# m2.borrow_book('Data Mining')
+# m1.borrow_book('Data Mining')
 # print(member_db)
+
+def show_book_db():
+    print()
+    for book, details in books_db.items():
+        print(book, '-', details)
+        print()
+
+if __name__ == "__main__":
+    show_book_db()
